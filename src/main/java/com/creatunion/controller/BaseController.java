3acc22e34894cb6020c7a2317727116fef2e6b33
fcx <fcxxiaohei@163.com>
2019-07-03 00:02:37 +0800

    测试1

--- src/main/java/com/creatunion/controller/BaseController.java
+++ src/main/java/com/creatunion/controller/BaseController.java
@@ -3,8 +3,6 @@ package com.creatunion.controller;
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("left");
-    }
-    public static void main(String[] args) {
         System.out.println("right");
     }
 }
