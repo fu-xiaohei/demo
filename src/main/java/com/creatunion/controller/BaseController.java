dd43520a0150e1935ab2067422a028ea11695018
fcx <fcxxiaohei@163.com>
2019-07-03 00:03:38 +0800

    右侧推送

--- src/main/java/com/creatunion/controller/BaseController.java
+++ src/main/java/com/creatunion/controller/BaseController.java
@@ -4,5 +4,6 @@ public class BaseController {
     public static void main(String[] args) {
         System.out.println("left");
         System.out.println("right");
+        System.out.println("right2");
     }
 }
